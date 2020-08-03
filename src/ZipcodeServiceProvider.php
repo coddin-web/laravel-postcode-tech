84214eae1bea60b96346a1e007c5dfa01dcf220d
Falko Woudstra <falko@coddin.nl>
2020-08-03 11:30:28 +0200

    wip

--- src/ZipcodeServiceProvider.php
+++ src/ZipcodeServiceProvider.php
@@ -13,19 +13,7 @@ class ZipcodeServiceProvider extends ServiceProvider
             $this->publishes([
                 __DIR__.'/../config/zipcode.php' => config_path('zipcode.php'),
             ], 'config');
-
-            $this->publishes([
-                __DIR__.'/../resources/views' => base_path('resources/views/vendor/zipcode'),
-            ], 'views');
-
-            if (! class_exists('CreatePackageTable')) {
-                $this->publishes([
-                    __DIR__ . '/../database/migrations/create_zipcode_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_zipcode_table.php'),
-                ], 'migrations');
-            }
         }
-
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'zipcode');
     }
 
     public function register()
